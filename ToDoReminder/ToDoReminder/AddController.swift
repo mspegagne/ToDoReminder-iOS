2fb5d0a6d7f740dbb6cb8ddcd7359f21aad2a56a
mspegagne <mathieu.sge@gmail.com>
2015-04-05 16:09:01 +0530

    add Core data ok !

--- ToDoReminder/ToDoReminder/AddController.swift
+++ ToDoReminder/ToDoReminder/AddController.swift
@@ -1,39 +1,49 @@
 //
-//  ViewController.swift
+//  AddController.swift
 //  ToDoReminder
 //
-//  Created by Mathieu Spegagne on 24/03/2015.
+//  Created by Mathieu Spegagne on 05/04/2015.
 //  Copyright (c) 2015 Mathieu Spegagne. All rights reserved.
 //
 
 import UIKit
-
+import CoreData
 
 class AddController: UIViewController {
+
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
     
-    @IBOutlet weak var TitleField: UITextField!
-    @IBOutlet weak var TextField: UITextField!
-    @IBOutlet weak var SaveButton: UIButton!
-    
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as AppDelegate).managedObjectContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func TitleListener(sender: AnyObject) {
+    @IBAction func saveListener(sender: AnyObject) {
+        if let moc = self.managedObjectContext {
+            let task = ToDo.save(moc, newTitle: titleField.text, newText: textField.text)
+            let alertController = task.alert()
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
     }
-    @IBAction func TextListener(sender: AnyObject) {
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
-    @IBAction func SaveListener(sender: AnyObject) {
-        var essai = ToDo(newText:TitleField.text, newTitle:TextField.text)
-        var alertController = essai.alert()
-        self.presentViewController(alertController, animated: true, completion: nil)
-    }
-    
+    */
+
 }
--- ToDoReminder/ToDoReminder/ToDo.swift
+++ ToDoReminder/ToDoReminder/ToDo.swift
@@ -2,29 +2,38 @@
 //  ToDo.swift
 //  ToDoReminder
 //
-//  Created by Mathieu Spegagne on 02/04/2015.
+//  Created by Mathieu Spegagne on 05/04/2015.
 //  Copyright (c) 2015 Mathieu Spegagne. All rights reserved.
 //
 
 import Foundation
+import CoreData
 import UIKit
 
-class ToDo {
-    
-    var Text:String
-    var Title:String
-    
-    init(newText:String, newTitle:String){
-        self.Text = newText
-        self.Title = newTitle
-    }
+@objc(ToDo)
+class ToDo: NSManagedObject {
+
+    @NSManaged var title: String
+    @NSManaged var text: String
+    @NSManaged var history: NSNumber
     
     func alert()->UIAlertController {
-        let alertController = UIAlertController(title: self.Title, message: self.Text, preferredStyle: UIAlertControllerStyle.Alert)
-    
+        let alertController = UIAlertController(title: self.title, message: self.text, preferredStyle: UIAlertControllerStyle.Alert)
+        
         alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default,handler: nil))
-    
+        
         return alertController
     }
     
+    class func save(moc: NSManagedObjectContext, newTitle: String, newText: String) -> ToDo {
+        
+        let task = NSEntityDescription.insertNewObjectForEntityForName("ToDo", inManagedObjectContext: moc) as ToDo
+        
+        task.title = newTitle
+        task.text = newText
+        task.history = false
+        
+        return task
+    }
+
 }
