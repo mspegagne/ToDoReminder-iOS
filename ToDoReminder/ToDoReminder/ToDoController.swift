8873eca69c69743e143b885476fccb0ef1bc4a06
mspegagne <mathieu.sge@gmail.com>
2015-04-07 14:20:48 +0530

    delete history

--- ToDoReminder/ToDoReminder/ToDoController.swift
+++ ToDoReminder/ToDoReminder/ToDoController.swift
@@ -30,18 +30,12 @@ class ToDoController: UITableViewController, NSFetchedResultsControllerDelegate 
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func fetchLog() {
         let fetchRequest = NSFetchRequest(entityName: "ToDo")
         
-        // Create a sort descriptor object that sorts on the "title"
-        // property of the Core Data object
-        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
-        
-        // Set the list of sort descriptors in the fetch request,
-        // so it includes the sort descriptor
+        let sortDescriptor = NSSortDescriptor(key: "history", ascending: true)
         fetchRequest.sortDescriptors = [sortDescriptor]
         
         if let fetchResults = managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [ToDo] {
@@ -56,7 +50,7 @@ class ToDoController: UITableViewController, NSFetchedResultsControllerDelegate 
     
     func taskFetchRequest() -> NSFetchRequest {
         let fetchRequest = NSFetchRequest(entityName: "ToDo")
-        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        let sortDescriptor = NSSortDescriptor(key: "history", ascending: true)
         fetchRequest.sortDescriptors = [sortDescriptor]
         return fetchRequest
     }
@@ -112,13 +106,10 @@ class ToDoController: UITableViewController, NSFetchedResultsControllerDelegate 
         managedObjectContext?.deleteObject(managedObject)
         managedObjectContext?.save(nil)
         
-        // Refresh the table view to indicate that it's deleted
         self.fetchLog()
         
-        // Tell the table view to animate out that row
         tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
         
-
     }
 
 }
